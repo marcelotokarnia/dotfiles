d50d5fa5e0d6001b2ec12011741cbedc19de9548
Marcelo Tokarnia <marcelo.tokarnia@klarna.com>
2020-08-19 08:42:32 +0200

    ådd fu abbr

--- fish/functions/setup.fish
+++ fish/functions/setup.fish
@@ -2,34 +2,35 @@ function setup
     for abbreviation in (abbr -l)
         abbr -e $abbreviation
     end
-    abbr g git
-    abbr c code .
+    abbr g "git"
+    abbr c "code ."
     
-    abbr gs git status
-    abbr ga git add .
-    abbr gc git commit -m
-    abbr gl git log
-    abbr gd git diff
-    abbr gds git diff --shortstat
+    abbr gs "git status"
+    abbr ga "git add ."
+    abbr gc "git commit -m"
+    abbr gl "git log"
+    abbr gd "git diff"
+    abbr gds "git diff --shortstat"
 
-    abbr gf git fetch
-    abbr gpl git pull --ff-only
-    abbr gp git push
-    abbr gpf git push --force
+    abbr gf "git fetch"
+    abbr gpl "git pull --ff-only"
+    abbr gp "git push"
+    abbr gpf "git push --force"
 
-    abbr gr git rebase origin/master
-    abbr grc git rebase --continue
-    abbr gra git rebase --abort
+    abbr gr "git rebase origin/master"
+    abbr grc "git rebase --continue"
+    abbr gra "git rebase --abort"
     
     
-    abbr gco git checkout
-    abbr gcm git checkout master
-    abbr gcb git checkout -
-    abbr ll ls -lhA
+    abbr gco "git checkout"
+    abbr gcm "git checkout master"
+    abbr gcb "git checkout -"
+    abbr ll "ls -lhA"
 
-    abbr t yarn test
-    abbr b yarn build
-    abbr bb yarn build:back
-    abbr bs yarn build & yarn start
-    abbr s yarn start
+    abbr t "yarn test"
+    abbr b "yarn build"
+    abbr bb "yarn build:back"
+    abbr bs "yarn build & yarn start"
+    abbr s "yarn start"
+    abbr fu "fnm use"
 end
